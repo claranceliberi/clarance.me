dd72a2879d2e72f3822434d3b818df8e3fcb3dc8
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2025-08-12 23:34:47 +0200

    highlight strettch cloud

--- pages/index.vue
+++ pages/index.vue
@@ -57,7 +57,7 @@ import { experiences, books } from '@/data/index';
                                 I fix things that piss me off. Usually that's slow software, broken infrastructure, or problems everyone says "can't be solved."
                                 <br />
 Africa-based engineering teams are stuck choosing between expensive, unreliable national cloud providers (who promise 98% SLA - the lowest in the world - then don't even meet it) or dealing with terrible latency to US servers. People have just accepted this is how it is.
-I'm building <a href="https://cloud.strettch.com" target="_blank" rel="noopener noreferrer">strettch cloud</a>, to fix it.
+I'm building <a class="underline" href="https://cloud.strettch.com" target="_blank" rel="noopener noreferrer">Strettch Cloud</a>, to fix it.
                             </p>
                         </div>
                         <div
